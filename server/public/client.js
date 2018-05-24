0b7aec296c0692df70806981f3074a44a74402e4
Jacob Tovsen <jacobtovsen@gmail.com>
2018-05-24 14:59:33 -0500

    full running.  included routers

--- server/public/client.js
+++ server/public/client.js
@@ -4,4 +4,62 @@ $(document).ready( onReady );
 
 function onReady(){
     console.log( 'jq' );
+    $("#add-record").on('click', function(event){
+        event.preventDefault();
+        //prevent default prevents page refresh on pressing submit button
+        
+        // addRecord( getNewRecord() );
+    })
+    getAllRecords();
+}
+
+function getNewRecord(){
+    let record = {
+        artist: $("#in-artist").val(),
+        album: $("#in-album").val(),
+        year: $("#in-year").val(),
+        genre: $("#in-genre").val(),
+    }
+    return record;
+}
+
+function addRecord(record){
+    $.ajax({
+        method: 'POST',
+        url: '/record',
+        data: record
+    }).then( function(response) {
+        //clear input fields
+        getAllRecords();
+    }).catch( function(response){
+        console.log('Something bad happened:', response.status );
+    });
+}
+
+function getAllRecords(){
+    $.ajax({
+        method: 'GET',
+        url: '/record'
+    }).then( function(response) {
+        displayAllRecords(response);
+    });
+}
+
+
+function displayAllRecords(recordArray){
+    let $recordsTarget = $('#records');
+    $recordsTarget.empty();
+    for(let record of recordArray){
+        $recordsTarget.append(makeRowFor(record));
+    }
+}
+
+function makeRowFor(record){
+    let rowHtml = `<tr>
+        <td>${record.artist}</td>
+        <td>${record.albumName}</td>
+        <td>${record.year}</td>
+        <td>${record.genreList.join(', ')}</td>
+    </tr>`;
+    return rowHtml;
 }
--- server/server.js
+++ server/server.js
@@ -1,25 +1,50 @@
 const express = require( 'express' );
 const bodyParser = require('body-parser');
-const Record = require('./modules/record.class');
+const recordRouter = require('./routers/record.router');
+// const Record = require('./modules/record.class');
+// sent to router
 
 const app = express();
 app.use( express.static( 'server/public' ));
 app.use(bodyParser.urlencoded( {extended: true} ) );
+app.use( '/record', recordRouter );
 
-//make some records
-const recordArray = [
-    new Record('Beatles', 'Abbey Road', 1969, ['rock']),
-    new Record('Michael Jackson', 'Off the Wall', 1979, ['Pop']),
-    new Record('Prince', 'Purple Rain', 1984, ['Pop']),
-    new Record('Cibo Matto', 'Via la Woman', 1990, ['Jpop'])
-];
-
-app.get('/record', ( req, res ) => {
-    console.log('Handling my GET for /record');
-    res.send(recordArray);
-})
 
 const port = 5000;
 app.listen( port, () => console.log( `listening on port ${port}`));
+
+
+
+//!!!!!!!sent all the following to router!!!!!!
+
+//make some records
+// const recordArray = [
+//     new Record('Beatles', 'Abbey Road', 1969, ['rock', 'pop', 'yippie skippy']),
+//     new Record('Michael Jackson', 'Off the Wall', 1979, ['Pop']),
+//     new Record('Prince', 'Purple Rain', 1984, ['Pop']),
+//     new Record('Cibo Matto', 'Via la Woman', 1990, ['Jpop'])
+// ];
+
+// app.get('/record', ( req, res ) => {
+//     console.log('Handling my GET for /record');
+//     res.send(recordArray);
+// });
+
+// app.post('/record', (req, res) => {
+//     console.log('Handling my POST for /record', req.body);
+//     let sentRecord = req.body;
+//     //I could make 100% sure the objects are exactly the same on the client
+//     // and server, or I could just assume they may not be and set all the values
+//     // again.  Nice if you aren't the one writing the code on both sides.
+//     let record = new Record(
+//         sentRecord.artist,
+//         sentRecord.album,
+//         sentRecord.year,
+//         [ sentRecord.genre ]
+//     );
+//     recordArray.push(record);
+//     res.sendStatus(201);
+//     // status 201 = 'created'
+// })
 
 
